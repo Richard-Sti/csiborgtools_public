963a3823624d865d4c636888a0d605669bc7a0c9
rstiskalek <richard.stiskalek@protonmail.com>
2023-12-13 16:20:10 +0000

    Remove old import

--- csiborgtools/read/halo_cat.py
+++ csiborgtools/read/halo_cat.py
@@ -29,7 +29,7 @@ from ..utils import (cartesian_to_radec, fprint, great_circle_distance,
                      number_counts, periodic_distance_two_points,
                      real2redshift)
 # TODO: removing these
-from .box_units import CSiBORG1Box, QuijoteBox
+# from .box_units import CSiBORG1Box, QuijoteBox
 from .paths import Paths
 
 ###############################################################################
