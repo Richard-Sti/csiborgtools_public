6c27e367167fd06e3287e6f473d9597c5d0f8c77
rstiskalek <richard.stiskalek@protonmail.com>
2024-06-28 12:44:16 +0100

    Update script

--- scripts/field_los.sh
+++ scripts/field_los.sh
@@ -6,20 +6,24 @@ env="/mnt/users/rstiskalek/csiborgtools/venv_csiborg/bin/python"
 file="field_los.py"
 
 simname=${1}
-catalogue=${2}
 nsims="-1"
 MAS="SPH"
 grid=1024
 
 
-pythoncm="$env $file --catalogue $catalogue --nsims $nsims --simname $simname --MAS $MAS --grid $grid"
-if [ $on_login -eq 1 ]; then
-    echo $pythoncm
-    $pythoncm
-else
-    cm="addqueue -q $queue -n $nthreads -m $memory $pythoncm"
-    echo "Submitting:"
-    echo $cm
-    echo
-    eval $cm
-fi
+
+for catalogue in "LOSS" "Foundation" "Pantheon+" "2MTF" "SFI_gals"; do
+    pythoncm="$env $file --catalogue $catalogue --nsims $nsims --simname $simname --MAS $MAS --grid $grid"
+    if [ $on_login -eq 1 ]; then
+        echo $pythoncm
+        $pythoncm
+    else
+        cm="addqueue -q $queue -n $nthreads -m $memory $pythoncm"
+        echo "Submitting:"
+        echo $cm
+        echo
+        eval $cm
+    fi
+
+    sleep 0.05
+done
